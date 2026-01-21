ee7292ba32c9600616c6471e2f994358138928c9
김예안 <kjan0929@naver.com>
2026-01-21 16:25:22 +0900

    feat(constant): 주요 매직 넘버,스트링 상수화

---
+++ constants.js
@@ -0,0 +1,11 @@
+// 화면 초기값
+export const INITIAL_DISPLAY_VALUE = '0';
+
+// 연산자 목록
+export const OPERATROS = ['+', '−', '×', '÷'];
+
+// 에러 메시지
+export const ERROR_MESSAGE = 'NaN';
+
+// 최대 입력 가능한 자릿수
+export const MAX_DIGIT_LENGTH = 10;
